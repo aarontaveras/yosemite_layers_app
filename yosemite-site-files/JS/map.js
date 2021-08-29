9333540985dc201a3f1a8176646961eccce55333
“your_github_username”ndfadmin <36459347+aarontaveras@users.noreply.github.com>
2021-08-29 09:11:52 -0700

    Update map.js

--- yosemite-site-files/JS/map.js
+++ yosemite-site-files/JS/map.js
@@ -355,8 +355,8 @@ map.on('click', function (e) {
 				'top-left': [0, 0],
 				'top-right': [0, 0],
 				'bottom': [0, -40],
-				'bottom-left': [0, -40],
-				'bottom-right': [0, -40],
+				'bottom-left': [0, -30],
+				'bottom-right': [0, -30],
 				'left': [18, -22],
 				'right': [-18, -22],
 			}
